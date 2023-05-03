f72ca3d802d7785ed79be4c49931de28cf6e965e
LloydN01 <lloydna0527@gmail.com>
2023-05-03 13:30:04 +0800

    update sql schema

--- schema.sql
+++ schema.sql
@@ -1,10 +1,11 @@
 DROP TABLE IF EXISTS chatHistory; /* Delete chatHistory table if it already exists */
 
 CREATE TABLE chatHistory (
-    chatId INTEGER PRIMARY KEY AUTOINCREMENT,
-    userID INTEGER NOT NULL,
-    botQuestion TEXT NOT NULL,
-    userResponse TEXT NOT NULL,
-    botReview TEXT NOT NULL,
-    timestamp TEXT NOT NULL
+    chatID INTEGER PRIMARY KEY AUTOINCREMENT, /* Surrogate key */
+    sessionID INTEGER NOT NULL, /* Groups the chat history by session */
+    userID INTEGER NOT NULL, /* Groups the chat history by user */
+    botQuestion TEXT NOT NULL, /* The question asked by the bot */
+    userResponse TEXT NOT NULL, /* The response given by the user */
+    botReview TEXT NOT NULL, /* The review of user answer given by the bot */
+    timestamp TEXT NOT NULL /* The timestamp of the chat */
 );
